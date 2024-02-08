b66c80f3a3248cccacdfbdf398bfa03cdceaa295
XPLOIT-123 <85129943+XPLOIT-123@users.noreply.github.com>
2024-02-08 23:47:12 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-function revealSecretMessage() {
-    var secretMessage = "This is a secret message! 🎉";
-    var messageList = document.getElementById("message-list");
-    var newMessage = document.createElement("div");
-    newMessage.textContent = secretMessage;
-    messageList.appendChild(newMessage);
-}
+
